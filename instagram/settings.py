c3728c98edb6f49faea465acf317944179e2c029
salah <0814604@gmail.com>
2026-05-12 15:41:37 +0100

    fix requirements

--- instagram/settings.py
+++ instagram/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'django-insecure-ik1_vhk9znd2u#!$j=oawx(2##^i)6p+)pdd)8av@9mp5ou)d1
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ["*"]
 
 # Application definition
 
@@ -120,7 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_ROOT = BASE_DIR / "staticfiles"  
 import os
 
 MEDIA_URL = '/media/'
